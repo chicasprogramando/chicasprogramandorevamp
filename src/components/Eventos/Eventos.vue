97ae3fb8932fe89c096ea9c7769d74beb0544707
GisellePerez <giselleperez91@gmail.com>
2018-07-23 23:43:48 -0300

    replaced buttons for img

--- src/components/Eventos/Eventos.vue
+++ src/components/Eventos/Eventos.vue
@@ -1,136 +1,133 @@
 <template>
   <v-container fluid class="main">
-      <v-layout align-space-between justify-space-between row fill-height height="100%">
-          <v-flex md6 class="left" height="100%" fill-height>
-            
-                <h1>Eventos y capacitaciones</h1>
-                <p class="subtitle">Buscamos hacer crecer a nuestras coders y a todos aquellos que deseen sumarse a nuestras capacitaciones y meetups.</p>
-            
-                <v-card class="cards">
-                  <v-container fluid grid-list-md>
-                      <v-layout row align-space-around>
-                        <v-flex xs10>
-                          <div>
-                            <span class="date">12/07/2018</span>
-                            <span class="place">Centro Cultural Recoleta</span>              
-                            <v-card-text class="event">Vue.js Meetup</v-card-text>
-                          </div>
-                        </v-flex>
-                        <v-flex xs2>
-                          <v-btn  fab dark right small color="purple">
-                            <v-icon>arrow_right</v-icon>
-                          </v-btn>
-                        </v-flex>
-                      </v-layout>
-                  </v-container>
-              </v-card>
+    <v-layout align-space-between justify-space-between row fill-height height="100%">
+      <v-flex md6 class="left" height="100%" fill-height>
+        <h1>Eventos y capacitaciones</h1>
+        <p class="subtitle">Buscamos hacer crecer a nuestras coders y a todos aquellos que deseen sumarse a nuestras capacitaciones y meetups.</p>
+        <v-card class="cards">
+          <v-container fluid grid-list-md>
+            <v-layout row align-content-space-between>
+              <v-flex sm11>
+                <div>
+                  <span class="date">12/07/2018</span>
+                  <span class="place">Centro Cultural Recoleta</span>              
+                  <v-card-text class="event">Vue.js Meetup</v-card-text>
+                </div>
+              </v-flex>
+              <v-flex sm1 class="arrow-div">
+                <figure>  
+                  <img src="../../assets/arrow-btn.png">  
+                </figure>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
 
-                <v-card class="cards">
-                  <v-container fluid grid-list-md>
-                      <v-layout row align-space-around>
-                        <v-flex xs10>
-                          <div>
-                            <span class="date">12/07/2018</span>
-                            <span class="place">Centro Cultural Recoleta</span>              
-                            <v-card-text class="event">Vue.js Meetup</v-card-text>
-                          </div>
-                        </v-flex>
-                        <v-flex xs2>
-                          <v-btn fab dark right small color="purple">
-                            <v-icon>arrow_right</v-icon>
-                          </v-btn>
-                        </v-flex>
-                      </v-layout>
-                  </v-container>
-                </v-card>
+        <v-card class="cards">
+          <v-container fluid grid-list-md>
+            <v-layout row align-content-space-between>
+              <v-flex sm11>
+                <div>
+                  <span class="date">12/07/2018</span>
+                  <span class="place">Centro Cultural Recoleta</span>              
+                  <v-card-text class="event">Vue.js Meetup</v-card-text>
+                </div>
+              </v-flex>
+              <v-flex sm1 class="arrow-div">
+                <figure>  
+                  <img src="../../assets/arrow-btn.png">  
+                </figure>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
 
-                <v-card class="cards">
-                  <v-container fluid grid-list-md>
-                      <v-layout row align-space-around>
-                        <v-flex xs10>
-                          <div>
-                            <span class="date">12/07/2018</span>
-                            <span class="place">Centro Cultural Recoleta</span>              
-                            <v-card-text class="event">Vue.js Meetup</v-card-text>
-                          </div>
-                        </v-flex>
-                        <v-flex xs2>
-                          <v-btn  fab dark right small color="purple">
-                            <v-icon>arrow_right</v-icon>
-                          </v-btn>
-                        </v-flex>
-                      </v-layout>
-                  </v-container>
-              </v-card>
+        <v-card class="cards">
+          <v-container fluid grid-list-md>
+            <v-layout row align-space-around>
+              <v-flex sm11>
+                <div>
+                  <span class="date">12/07/2018</span>
+                  <span class="place">Centro Cultural Recoleta</span>              
+                  <v-card-text class="event">Vue.js Meetup</v-card-text>
+                </div>
+              </v-flex>
+              <v-flex sm1 class="arrow-div">
+                <figure>  
+                  <img src="../../assets/arrow-btn.png">  
+                </figure>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
 
-              <v-card class="cards">
-                  <v-container fluid grid-list-md>
-                      <v-layout row align-space-around>
-                        <v-flex xs10>
-                          <div>
-                            <span class="date">12/07/2018</span>
-                            <span class="place">Centro Cultural Recoleta</span>              
-                            <v-card-text class="event">Vue.js Meetup</v-card-text>
-                          </div>
-                        </v-flex>
-                        <v-flex xs2>
-                          <v-btn  fab dark right small color="purple">
-                            <v-icon>arrow_right</v-icon>
-                          </v-btn>
-                        </v-flex>
-                      </v-layout>
-                  </v-container>
-              </v-card>
+        <v-card class="cards">
+          <v-container fluid grid-list-md>
+            <v-layout row align-space-around>
+              <v-flex sm11>
+                <div>
+                  <span class="date">12/07/2018</span>
+                  <span class="place">Centro Cultural Recoleta</span>              
+                  <v-card-text class="event">Vue.js Meetup</v-card-text>
+                </div>
+              </v-flex>
+              <v-flex sm1 class="arrow-div">
+                <figure>  
+                  <img src="../../assets/arrow-btn.png">  
+                </figure>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
 
-                <v-card class="cards">
-                  <v-container fluid grid-list-md>
-                      <v-layout row align-space-around>
-                        <v-flex xs10>
-                          <div>
-                            <span class="date">12/07/2018</span>
-                            <span class="place">Centro Cultural Recoleta</span>              
-                            <v-card-text class="event">Vue.js Meetup</v-card-text>
-                          </div>
-                        </v-flex>
-                        <v-flex xs2 justify-end>
-                          <v-btn  fab dark right small color="purple">
-                            <v-icon>arrow_right</v-icon>
-                          </v-btn>
-                        </v-flex>
-                      </v-layout>
-                  </v-container>
-                </v-card>
+        <v-card class="cards">
+          <v-container fluid grid-list-md>
+            <v-layout row align-space-around>
+              <v-flex sm11>
+                <div>
+                  <span class="date">12/07/2018</span>
+                  <span class="place">Centro Cultural Recoleta</span>              
+                  <v-card-text class="event">Vue.js Meetup</v-card-text>
+                </div>
+              </v-flex>
+              <v-flex sm1 class="arrow-div">
+                <figure>  
+                  <img src="../../assets/arrow-btn.png">  
+                </figure>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
 
-            </v-flex>
+      </v-flex>
 
-          <v-card sm6 class="right">
-              <h2>Otras capacitaciones</h2>
+      <v-card sm6 class="right" fill-height>
+          <h2>Otras capacitaciones</h2>
 
-              <v-card flat class="twits">
-                <v-card-text  class="text-xs-center">Twit</v-card-text>
-              </v-card>
-
-              <v-card flat class="twits">
-                <v-card-text class="text-xs-center">Twit</v-card-text>
-              </v-card>
-            
-              <v-card flat class="twits">
-                <v-card-text class="text-xs-center">Twit</v-card-text>
-              </v-card>
-        
-              <v-card flat class="twits">
-                <v-card-text class="text-xs-center">Twit</v-card-text>
-              </v-card>
-            
-              <v-card flat class="twits">
-                <v-card-text class="text-xs-center">Twit</v-card-text>
-              </v-card>
-
-              <v-card flat class="twits">
-                <v-card-text class="text-xs-center">Twit</v-card-text>
-              </v-card>
+          <v-card flat class="twits">
+            <v-card-text  class="text-xs-center">Twit</v-card-text>
           </v-card>
-      
+
+          <v-card flat class="twits">
+            <v-card-text class="text-xs-center">Twit</v-card-text>
+          </v-card>
+        
+          <v-card flat class="twits">
+            <v-card-text class="text-xs-center">Twit</v-card-text>
+          </v-card>
+    
+          <v-card flat class="twits">
+            <v-card-text class="text-xs-center">Twit</v-card-text>
+          </v-card>
+        
+          <v-card flat class="twits">
+            <v-card-text class="text-xs-center">Twit</v-card-text>
+          </v-card>
+
+          <v-card flat class="twits">
+            <v-card-text class="text-xs-center">Twit</v-card-text>
+          </v-card>
+        </v-card>
       </v-layout>
   </v-container>
 </template>
@@ -153,7 +150,7 @@ export default {
   padding: 0;
 }
 .left{
-  padding: 1.5em 1.5em 0 1.5em;
+  padding: 1.5em;
 }
 .right{
   background-color:#7e64ab;
@@ -193,15 +190,27 @@ h2{
   color:#ffffff;
 }
 .cards{
-  margin: 1em 0;
-  padding: 0.4em;
+  margin: 0.9em 0;
+  padding: 0.3em;
+  display: flex;
+  justify-content: space-between;
+  align-content:center;
 }
 .twits{
-  margin:0.8em 0;
+  margin:0.7em 0;
   padding: 0.5em;
   opacity:0.9;
 }
 .event{
   padding:1em 0 0 0;
+}
+.next-btn{
+  text-align: right;
+  display: flex;
+  justify-content: flex-end;
+}
+.arrow-div{
+  margin-top: 0.5em;
+  cursor:pointer;
 }
 </style>
