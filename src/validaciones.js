1f24b20c2da52b75204845a42d369a38a5035472
CindyPotes <cindyjhidalgo@hotmail.com>
2018-07-25 18:25:45 -0300

    maquetacion pag. contacto

---
+++ src/validaciones.js
@@ -0,0 +1,16 @@
+export const nombreRules = [
+  v => !!v || 'Nombre requerido',
+  v => v.length <= 40 || 'El nombre debe tener menos de 40 caracteres'
+]
+export const emailRules = [
+    v => !!v || 'E-mail requerido',
+    v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+]
+export const campoRequeridoRules = [
+  v => !!v || 'Campo requerido',
+]
+
+export const descripcionRules = [
+  v => v.length <= 500 || 'El texto puede tener hasta 500 caracteres',
+  v => !!v || 'Campo requerido'
+]
