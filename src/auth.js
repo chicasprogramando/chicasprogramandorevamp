d43a369b8fa26b6685757ff9504810a7a79f8e54
puribey <kenshipichu@gmail.com>
2019-07-13 11:47:32 -0300

    feat: get email from auth0

--- src/auth.js
+++ src/auth.js
@@ -8,9 +8,9 @@ let webAuth = new auth0.WebAuth({
   // make sure this line is contains the port: 8080
   redirectUri: process.env.AUTH_REDIRECT_URI,
   // we will use the api/v2/ to access the user information as payload
-  audience: process.env.AUTH_AUDIENCE, 
+  audience: process.env.AUTH_AUDIENCE,
   responseType: 'token id_token',
-  scope: 'openid profile' // define the scopes you want to use
+  scope: 'openid email profile' // define the scopes you want to use and gets email
 })
 
 let auth = new Vue({
@@ -54,14 +54,14 @@ let auth = new Vue({
       webAuth.authorize()
     },
     logout() {
-      return new Promise((resolve, reject) => { 
+      return new Promise((resolve, reject) => {
         localStorage.removeItem('access_token')
         localStorage.removeItem('id_token')
         localStorage.removeItem('expires_at')
         localStorage.removeItem('user')
         webAuth.logout({
           returnTo: 'http://localhost:8080/#/login', // Allowed logout URL listed in dashboard
-          clientID: process.env.AUTH_CLIENT_ID, // Your client ID
+          clientID: process.env.AUTH_CLIENT_ID // Your client ID
         })
       })
     },
@@ -69,21 +69,18 @@ let auth = new Vue({
       return new Date().getTime() < this.expiresAt
     },
     handleAuthentication() {
-      return new Promise((resolve, reject) => {  
+      return new Promise((resolve, reject) => {
         webAuth.parseHash((err, authResult) => {
-
           if (authResult && authResult.accessToken && authResult.idToken) {
             this.expiresAt = authResult.expiresIn
             this.accessToken = authResult.accessToken
             this.token = authResult.idToken
             this.user = authResult.idTokenPayload
             resolve()
-
           } else if (err) {
             this.logout()
             reject(err)
           }
-
         })
       })
     }
