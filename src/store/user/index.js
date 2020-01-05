ed56dacf7b46c0471f7cc05b95f22ccc80e21f21
puribey <kenshipichu@gmail.com>
2020-01-05 17:03:17 -0300

    refactor: user store module changed

--- src/store/user/index.js
+++ src/store/user/index.js
@@ -1,10 +1,88 @@
+import axios from "axios";
+import router from "../../router";
+
 const state = {
   user: {}
 };
 
-const mutations = {};
-const actions = {};
-const getters = {};
+const mutations = {
+  SET_USER_INFO(state, payload) {
+    state.user = payload;
+  }
+};
+const actions = {
+  getUser(context, payload) {
+    const { name, nickname, email, sub, given_name } = payload;
+    axios
+      .get(`${process.env.VUE_APP_API_URL}/api/user/${sub}`)
+      .then(res => {
+        const { data } = res.data;
+        context.commit("SET_USER_INFO", data);
+        if (!data.accepted_terms) {
+          router.replace("/terms");
+        } else if (!data.completed_profile) {
+          router.replace("/profile");
+        } else {
+          router.replace("/");
+        }
+      })
+      .catch(e => {
+        const { status } = e.response;
+        if (status === 404) {
+          context.dispatch("createUser", {
+            user_name: nickname || name || given_name,
+            auth_sub: sub,
+            email: email
+          });
+        } else {
+          // eslint-disable-next-line
+          console.log(e);
+        }
+      });
+  },
+  createUser(context, payload) {
+    axios
+      .post(`${process.env.VUE_APP_API_URL}/api/user/`, payload)
+      .then(res => {
+        const { data } = res.data;
+        context.commit("SET_USER_INFO", data);
+        if (!data.accepted_terms) {
+          router.replace("/terms");
+        } else if (!data.completed_profile) {
+          router.replace("/profile");
+        } else {
+          router.replace("/");
+        }
+      })
+      .catch(e => {
+        // eslint-disable-next-line
+        console.log(e);
+      });
+  },
+  acceptedTerms(context, payload) {
+    const user = context.getters.getUserData;
+    axios
+      .put(`${process.env.VUE_APP_API_URL}/api/user/${user.id}`, payload)
+      .then(res => {
+        // eslint-disable-next-line
+        console.log("acceptedTerms", res);
+        if (!user.completed_profile) {
+          router.replace("/profile");
+        } else {
+          router.replace("/");
+        }
+      })
+      .catch(e => {
+        // eslint-disable-next-line
+        console.log(e);
+      });
+  }
+};
+const getters = {
+  getUserData: state => {
+    return state.user;
+  }
+};
 
 const user = {
   state,
