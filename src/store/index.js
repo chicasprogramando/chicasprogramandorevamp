a5b52187d046c087d1306e38e20425f8a7882135
puribey <kenshipichu@gmail.com>
2020-01-15 09:26:14 -0300

    feat: adding a little bit of error handling to the app

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from "vue";
 import Vuex from "vuex";
 import auth from "./auth";
 import user from "./user";
+import errors from "./errors";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     auth,
-    user
+    user,
+    errors
   }
 });
