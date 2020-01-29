fdd150e4892214a25de0959619ad24f66df9e4fe
puribey <kenshipichu@gmail.com>
2020-01-29 16:40:14 -0300

    refactor: simplify router with terms acceptance

--- src/router.js
+++ src/router.js
@@ -30,7 +30,7 @@ const router = new Router({
       path: "/profile",
       name: "Profile",
       component: Profile,
-      meta: { requiresAuth: true, requiresTerms: true }
+      meta: { requiresAuth: true }
     },
     {
       path: "/login",
@@ -87,20 +87,6 @@ router.beforeEach((to, from, next) => {
     }
   } else {
     next();
-  }
-
-  if (to.matched.some(record => record.meta.requiresTerms)) {
-    const user = Store.getters.getUserData;
-    // TODO: when refreshing the page in /profile user data comes undefined and redirects to /terms
-    if (user) {
-      if (user.accepted_terms) {
-        next();
-      } else {
-        Store.dispatch("userAcceptanceFlow");
-      }
-    } else {
-      next();
-    }
   }
 });
 
