de9a798159491403db7a139b563f3a5dae91ba6c
puribey <kenshipichu@gmail.com>
2020-01-29 16:42:42 -0300

    chore: prettier

--- src/utils/validaciones.js
+++ src/utils/validaciones.js
@@ -1,19 +1,20 @@
-import {isNil} from 'ramda'
+import { isNil } from "ramda";
 
 export const nombreRules = [
-  v => !!v || 'Nombre requerido',
-  v => v.length <= 40 || 'El nombre debe tener menos de 40 caracteres'
-]
+  v => !!v || "Nombre requerido",
+  v => v.length <= 40 || "El nombre debe tener menos de 40 caracteres"
+];
 export const emailRules = [
-    v => !!v || 'E-mail requerido',
-    v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
-]
-export const campoRequeridoRules = [
-  v => !!v || 'Campo requerido',
-]
+  v => !!v || "E-mail requerido",
+  v =>
+    /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+    "E-mail must be valid"
+];
+export const campoRequeridoRules = [v => !!v || "Campo requerido"];
 
 export const descripcionRules = [
-  v => !isNil(v) && v.length <= 500 || 'El texto puede tener hasta 500 caracteres',
-  v => !!v || 'Campo requerido'
-]
-
+  v =>
+    (!isNil(v) && v.length <= 500) ||
+    "El texto puede tener hasta 500 caracteres",
+  v => !!v || "Campo requerido"
+];
