4d8762b00dfb754fd931b34ab363d20ac6dbf39a
puribey <kenshipichu@gmail.com>
2020-03-17 14:30:28 -0300

    feat: adding Sentry

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import axios from "axios";
 import VueAxios from "vue-axios";
+import * as Sentry from "@sentry/browser";
+import * as Integrations from "@sentry/integrations";
+
+Sentry.init({
+  dsn: process.env.VUE_APP_SENTRY_DNS,
+  integrations: [new Integrations.Vue({ Vue, attachProps: true })]
+});
 
 Vue.use(VueAxios, axios);
 Vue.config.productionTip = false;
