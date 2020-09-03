83cc89f2f6017ad40c793874357a31579bbf5dcb
Eleonora Lester <eleonora.lester@gmail.com>
2020-09-03 09:45:17 -0300

    clear

--- src/components/_common/index.jsx
+++
@@ -1,3 +0,0 @@
-export { default as Header } from "../_common/Header/index";
-export { default as BannerCarousel } from "../_common/BannerCarousel/index";
-export { default as Footer } from "../_common/Footer/index";
